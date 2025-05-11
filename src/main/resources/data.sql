5d563b353845008c6deb95cad4bc6df3c65df3b3
Andrii Ovcharuk <phent1soff@gmail.com>
2025-05-11 19:48:42 +0300

    UPDATE: new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO users (id, first_name, last_name, email, password, role) VALUES (2, 'Harley', 'Almond', 'harley@mail.com', '$2a$12$hBxKD42i90mKap1S3oa.D.eLE3RNWwW5ieXrEV3C8X22ANCmGBJte', 'USER');
-INSERT INTO users (id, first_name, last_name, email, password, role) VALUES (3, 'Bill', 'Black', 'bill@mail.com', '$2a$12$M8ejDjJliggKWUodFncD9.sZyMrhC7PpsmaYXb5i7iPamZ6/eytGa', 'USER');
 INSERT INTO users (id, first_name, last_name, email, password, role) VALUES (1, 'Alex', 'Fisher', 'alex@mail.com', '$2a$12$n7JxG2mB4x2QFv5oDu.X1.T8dUxqCHmIyZ6vsRgyS/eFvjlT44VwS', 'ADMIN');
+INSERT INTO users (id, first_name, last_name, email, password, role) VALUES (2, 'Bill', 'Black', 'bill@mail.com', '$2a$12$M8ejDjJliggKWUodFncD9.sZyMrhC7PpsmaYXb5i7iPamZ6/eytGa', 'ADMIN');
+INSERT INTO users (id, first_name, last_name, email, password, role) VALUES (3, 'Harley', 'Almond', 'harley@mail.com', '$2a$12$hBxKD42i90mKap1S3oa.D.eLE3RNWwW5ieXrEV3C8X22ANCmGBJte', 'USER');
 
 
 INSERT INTO states (id, name) VALUES (1, 'New');
@@ -12,10 +12,10 @@ INSERT INTO states (id, name) VALUES (4, 'Done');
 INSERT INTO todos (id, title, created_at, owner_id) VALUES (1, 'Alex''s To-Do #1', '2024-10-06 15:00:04.114253', 1);
 INSERT INTO todos (id, title, created_at, owner_id) VALUES (2, 'Alex''s To-Do #2', '2024-10-06 15:00:11.585311', 1);
 INSERT INTO todos (id, title, created_at, owner_id) VALUES (3, 'Alex''s To-Do #3', '2024-10-06 15:00:16.952515', 1);
-INSERT INTO todos (id, title, created_at, owner_id) VALUES (4, 'Harley''s To-Do #1', '2024-10-06 15:14:54.532337', 2);
-INSERT INTO todos (id, title, created_at, owner_id) VALUES (5, 'Harley''s To-Do #2', '2024-10-06 15:15:04.707176', 2);
-INSERT INTO todos (id, title, created_at, owner_id) VALUES (6, 'Bill''s To-Do #1', '2024-10-06 15:15:32.464391', 3);
-INSERT INTO todos (id, title, created_at, owner_id) VALUES (7, 'Bill''s To-Do #2', '2024-10-06 15:15:39.16246', 3);
+INSERT INTO todos (id, title, created_at, owner_id) VALUES (4, 'Bill''s To-Do #1', '2024-10-06 15:14:54.532337', 2);
+INSERT INTO todos (id, title, created_at, owner_id) VALUES (5, 'Bill''s To-Do #2', '2024-10-06 15:15:04.707176', 2);
+INSERT INTO todos (id, title, created_at, owner_id) VALUES (6, 'Harley''s To-Do #1', '2024-10-06 15:15:32.464391', 3);
+INSERT INTO todos (id, title, created_at, owner_id) VALUES (7, 'Harley''s To-Do #2', '2024-10-06 15:15:39.16246', 3);
 
 INSERT INTO tasks (id, name, priority, todo_id, state_id) VALUES (2, 'Task #2', 'LOW', 1, 1);
 INSERT INTO tasks (id, name, priority, todo_id, state_id) VALUES (1, 'Task #1', 'HIGH', 1, 4);
