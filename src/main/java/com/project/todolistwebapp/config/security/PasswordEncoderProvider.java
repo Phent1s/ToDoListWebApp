8e2df1058fa15c813db1252b01b37542e57fbe08
Andrii Ovcharuk <phent1soff@gmail.com>
2025-05-11 18:29:32 +0300

    changed to bcrypt

--- src/main/java/com/project/todolistwebapp/config/security/PasswordEncoderProvider.java
+++ src/main/java/com/project/todolistwebapp/config/security/PasswordEncoderProvider.java
@@ -2,6 +2,7 @@ package com.project.todolistwebapp.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -9,6 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordEncoderProvider {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
